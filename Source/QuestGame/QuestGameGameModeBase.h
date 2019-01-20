1cf1f1584d7e1ba837372920093effaf857d9f3a
Naveen <mr.royya@gmail.com>
2019-01-20 15:40:53 +0530

    Add project files.

---
+++ Source/QuestGame/QuestGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QuestGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUESTGAME_API AQuestGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
